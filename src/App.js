233b936a0a3da7dcd4b27a656479ca19eb60b2b7
Syyad Khan <syykhan@deloitte.com>
2021-10-11 15:06:06 -0400

    context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={HomePage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/CoinPage.js
@@ -0,0 +1,5 @@
+const CoinPage = () => {
+  return <div>coin page</div>;
+};
+
+export default CoinPage;
